fe54b364bc7f8f9ed2cba6032ebc2ff73450cc56
robertome <roberto.martin.escribano@alumnos.upm.es>
2019-04-22 19:08:03 +0200

    Add Orders Controller to List and see Details. Autorize annotation added to control unregistred admin users

---
+++ TiendaVirtualWeb/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVirtualWeb.Data;
+
+namespace TiendaVirtualWeb.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private TiendaVirtualDbEntities db = new TiendaVirtualDbEntities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
